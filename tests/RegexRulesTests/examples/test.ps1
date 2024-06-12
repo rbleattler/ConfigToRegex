7211872b1a744313d1cd8d10513fb4b4182d941e
Rob Bleattler <rbleattler@outlook.com>
2024-06-12 07:58:51 -0400

    refactor: Update PowerShell test script to include new pattern files

--- tests/RegexRulesTests/examples/test.ps1
+++ tests/RegexRulesTests/examples/test.ps1
@@ -5,25 +5,33 @@ param(
 
 $root = $PSScriptRoot
 
+Push-Location $PSScriptRoot\..\..\..\
 if (-not $skipBuild) {
-  Set-Location $PSScriptRoot\..\ && dotnet clean && dotnet build --configuration release && dotnet publish &&
+  dotnet clean && dotnet build --configuration release && dotnet publish &&
   Import-Module .\src\RegexRules\bin\Release\net8.0\publish\RegexRules.dll
 } else {
-  try { Import-Module .\src\RegexRules\bin\Release\net8.0\publish\RegexRules.dll }catch {
+  try {
+    Import-Module .\src\RegexRules\bin\Release\net8.0\publish\RegexRules.dll
+  } catch {
     Write-Warning "Please run the script without the -skipBuild flag first"
   }
 }
+Pop-Location
 $testPattern = Get-Content -Raw "$root\basicpattern.yml"
 $groupPattern = Get-Content -Raw "$root\grouppattern.yml"
 $groupPattern2 = Get-Content -Raw "$root\grouppattern2.yml"
 $patternValue = Get-Content -Raw "$root\patternvalue.yml"
 $testQuantifier = Get-Content -Raw "$root\quantifier.yml"
+$patternProperties = Get-Content -Raw "$root\patternproperties.yml"
+$anchorPattern = Get-Content -Raw "$root\anchorpattern.yml"
 
 $pv = [RegexRules.PatternValue]::new($patternValue)
 $q = [RegexRules.Quantifier]::new($testQuantifier)
 $p = [RegexRules.Pattern]::new($testPattern)
 $gp = [RegexRules.GroupPattern]::new($groupPattern)
 $gp2 = [RegexRules.GroupPattern]::new($groupPattern2)
+$pp = [RegexRules.PatternProperties]::new($patternProperties)
+$ap = [RegexRules.AnchorPattern]::new($anchorPattern)
 
 Write-Output "PatternValues:"
 $pv | Format-List *
@@ -34,3 +42,7 @@ $p | Format-List *
 Write-Output "Group Patterns:"
 $gp | Format-List *
 $gp2 | Format-List *
+Write-Output "Pattern Properties:"
+$pp | Format-List *
+Write-Output "Anchor Patterns:"
+$ap | Format-List *
