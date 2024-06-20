30714d9248eb2db74a9d08af63e450711a6ca722
Rob Bleattler <rbleattler@outlook.com>
2024-06-20 07:17:07 -0400

    refactor: Add IRegexSerializable interface for YAML and JSON serialization

---
+++ src/RegexRules/Interfaces/IRegexSerializable.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using YamlDotNet.Serialization;
+
+namespace RegexRules;
+
+public interface IRegexSerializable
+{
+
+    string SerializeYaml();
+
+    string SerializeJson();
+
+    IRegexSerializable DeserializeYaml(string yamlString);
+
+    IRegexSerializable DeserializeJson(string jsonString);
+
+}
