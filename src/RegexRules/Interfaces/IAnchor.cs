e4d03b7fbec06e72ce004b572c4c292b73ec0320
rbleattler <rbleattler@outlook.com>
2024-06-21 11:42:38 -0400

    refactor: Remove unused deserialization methods from IAnchor interface

--- src/RegexRules/Interfaces/IAnchor.cs
+++ src/RegexRules/Interfaces/IAnchor.cs
@@ -26,30 +26,4 @@ public interface IAnchor : IPattern, IRegexSerializable
     [YamlIgnore]
     JsonSchema IPattern.JsonSchema => JsonSchema.FromType(GetType());
 
-
-    private void DeserializeYaml(string anchorObjectPattern)
-    {
-        var deserializer = new Deserializer();
-        var pattern = deserializer.Deserialize<AnchorPattern>(anchorObjectPattern) ?? throw new Exception("Invalid YAML");
-
-        Id = pattern.Id;
-        Message = pattern.Message;
-        Type = pattern.Type;
-        Value = pattern.Value;
-        Properties = pattern.Properties;
-        Quantifiers = pattern.Quantifiers;
-    }
-
-    private void DeserializeJson(string anchorObjectPattern)
-    {
-        var pattern = JsonSerializer.Deserialize<AnchorPattern>(anchorObjectPattern) ?? throw new Exception("Invalid JSON");
-
-        Id = pattern.Id;
-        Message = pattern.Message;
-        Type = pattern.Type;
-        Value = pattern.Value;
-        Properties = pattern.Properties;
-        Quantifiers = pattern.Quantifiers;
-    }
-
 }
