846a599071eec17a1dad1383ccaf268ee13d6ddd
Rob Bleattler <rbleattler@outlook.com>
2024-06-28 15:20:47 -0400

    document: Update CharacterClassPattern.cs documentation

--- src/ConfigToRegex/Models/CharacterClassPattern.cs
+++ src/ConfigToRegex/Models/CharacterClassPattern.cs
@@ -17,6 +17,8 @@ public class CharacterClassPattern : ICharacterClass
     private PatternValue _value = new(string.Empty);
     private string _type = "CharacterClass";
 
+    // Inherited Properties
+
     string? IPattern.Id { get => Id; set => Id = value; }
     IPatternValue IPattern.Value
     {
@@ -36,10 +38,18 @@ public class CharacterClassPattern : ICharacterClass
         set => Type = value;
     }
 
+    // Properties
+
+    /// <summary>
+    /// The unique identifier for the pattern
+    /// </summary>
     [JsonPropertyName("Id")]
     [YamlMember(Alias = "Id")]
     public string? Id { get; set; } = Guid.NewGuid().ToString();
 
+    /// <summary>
+    /// The type of pattern
+    /// </summary>
     [JsonPropertyName("Type")]
     [YamlMember(Alias = "Type")]
     public string Type
@@ -48,6 +58,10 @@ public class CharacterClassPattern : ICharacterClass
         set => _ = value;
     }
 
+    /// <summary>
+    /// The value of the pattern
+    /// </summary>
+    /// <value><see cref="PatternValue"/></value>
     [JsonPropertyName("Value")]
     [YamlMember(Alias = "Value")]
     public PatternValue Value
@@ -84,18 +98,34 @@ public class CharacterClassPattern : ICharacterClass
         }
     }
 
+
+    /// <summary>
+    /// The <see cref="Quantifier"/>s for the pattern.
+    /// </summary>
+    /// <value><see cref="Quantifier"/></value>
     [JsonPropertyName("Quantifiers")]
     [YamlMember(Alias = "Quantifiers")]
     public Quantifier? Quantifiers { get; set; }
 
+
+    /// <summary>
+    /// The message for the pattern. This is an optional field, used to provide additional information about the pattern.
+    /// </summary>
+    /// <value><see cref="string"/></value>
     [JsonPropertyName("Message")]
     [YamlMember(Alias = "Message")]
     public string? Message { get; set; }
 
+    /// <summary>
+    /// The <see cref="PatternProperties"/> for the pattern. This is not required except on <see cref="GroupPattern"/>s.
+    /// </summary>
     [JsonPropertyName("Properties")]
     [YamlMember(Alias = "Properties")]
     public IPatternProperties? Properties { get; set; }
 
+    /// <summary>
+    /// The <see cref="JsonSchema"/> for the pattern. This is a generated schema based on the properties of the pattern.
+    /// </summary>
     [JsonIgnore]
     [YamlIgnore]
     public JsonSchema JsonSchema => JsonSchema.FromType(GetType());
@@ -153,7 +183,15 @@ public class CharacterClassPattern : ICharacterClass
         }
     }
 
-    public override string? ToString() => Value!.ToString() ?? string.Empty;
+    void IRegexSerializable.DeserializeYaml(string yamlString)
+    {
+        DeserializeYaml(yamlString);
+    }
+
+    void IRegexSerializable.DeserializeJson(string jsonString)
+    {
+        DeserializeJson(jsonString);
+    }
 
     internal bool IsJson(string patternObject) => ((IPattern)this).IsJson(patternObject);
 
@@ -231,30 +269,6 @@ public class CharacterClassPattern : ICharacterClass
         }
     }
 
-    public static string GetCharacterClass(string value)
-    {
-        // get the value of the appropriate CharacterClass from the static class CharacterClasses in FluentRegex
-        string CharacterClass = default!;
-        var validTypes = GetValidCharacterClassTypes();
-        if (validTypes.Contains(value))
-        {
-            CharacterClass = (string)typeof(CharacterClasses).GetField(value)!.GetValue(null)!;
-        }
-        if (string.IsNullOrWhiteSpace(CharacterClass))
-        {
-            var validValues = GetValidCharacterClassValues();
-            if (validValues.Contains(value))
-            {
-                CharacterClass = value;
-            }
-        }
-        if (string.IsNullOrWhiteSpace(CharacterClass))
-        {
-            throw new ArgumentException("Invalid CharacterClass Type. Valid types are: " + string.Join(", ", GetValidCharacterClassTypes()));
-        }
-        return CharacterClass;
-    }
-
     private static List<string?> GetValidCharacterClassValues()
     {
         return typeof(CharacterClasses).GetFields().Select(f => f.GetValue(null)!.ToString()).ToList()!;
@@ -303,6 +317,48 @@ public class CharacterClassPattern : ICharacterClass
         return type == "Literal" || type == "CharacterClass" || type == "CharacterClass" || type == "Group";
     }
 
+    // public methods
+
+    /// <summary>
+    /// Returns a string representation of the pattern (the <see cref="Value"/>) of the pattern.)
+    /// </summary>
+    /// <returns></returns>
+    public override string? ToString() => Value!.ToString() ?? string.Empty;
+
+    /// <summary>
+    /// Get's the value of the appropriate CharacterClass from the static class <see cref="CharacterClass" /> from the FluentRegex library.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns><see cref="string"/></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static string GetCharacterClass(string value)
+    {
+        // get the value of the appropriate CharacterClass from the static class CharacterClasses in FluentRegex
+        string CharacterClass = default!;
+        var validTypes = GetValidCharacterClassTypes();
+        if (validTypes.Contains(value))
+        {
+            CharacterClass = (string)typeof(CharacterClasses).GetField(value)!.GetValue(null)!;
+        }
+        if (string.IsNullOrWhiteSpace(CharacterClass))
+        {
+            var validValues = GetValidCharacterClassValues();
+            if (validValues.Contains(value))
+            {
+                CharacterClass = value;
+            }
+        }
+        if (string.IsNullOrWhiteSpace(CharacterClass))
+        {
+            throw new ArgumentException("Invalid CharacterClass Type. Valid types are: " + string.Join(", ", GetValidCharacterClassTypes()));
+        }
+        return CharacterClass;
+    }
+
+    /// <summary>
+    /// Serializes the <see cref="CharacterClassPattern"/> to a YAML string.
+    /// </summary>
+    /// <returns><see cref="string"/></returns>
     public string SerializeYaml()
     {
         var serializer = new SerializerBuilder().Build();
@@ -310,22 +366,20 @@ public class CharacterClassPattern : ICharacterClass
         return yaml;
     }
 
+    /// <summary>
+    /// Serializes the <see cref="CharacterClassPattern"/> to a JSON string.
+    /// </summary>
+    /// <returns><see cref="string"/></returns>
     public string SerializeJson()
     {
         var json = JsonSerializer.Serialize(this);
         return json;
     }
 
-    void IRegexSerializable.DeserializeYaml(string yamlString)
-    {
-        DeserializeYaml(yamlString);
-    }
-
-    void IRegexSerializable.DeserializeJson(string jsonString)
-    {
-        DeserializeJson(jsonString);
-    }
-
+    /// <summary>
+    /// Returns the pattern as a regular expression string.
+    /// </summary>
+    /// <returns><see cref="string"/></returns>
     public string ToRegex()
     {
         var regex = Value.ToString();
