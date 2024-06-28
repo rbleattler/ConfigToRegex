bc2ce8c798b0a2de947d26b65ec42622f2cc3350
Rob Bleattler <rbleattler@outlook.com>
2024-06-28 15:33:09 -0400

    refactor: Update PatternValue documentation

--- src/ConfigToRegex/Models/PatternValue.cs
+++ src/ConfigToRegex/Models/PatternValue.cs
@@ -7,19 +7,32 @@ using System.Text.RegularExpressions;
 
 namespace ConfigToRegex;
 
+/// <summary>
+/// Represents the value of a Regular Expression Pattern as an object. This is used on all <see cref="IPattern"/> objects.
+/// </summary>
 [YamlSerializable]
 
 public class PatternValue : IPatternValue
 {
 
+  /// <summary>
+  /// The value of the pattern. This can be a literal value, like a <see cref="string"/>, <see cref="char"/>, or another <see cref="PatternValue"/> object.
+  /// </summary>
   [JsonPropertyName("Value")]
   [YamlMember(Alias = "Value", Description = "The value of the pattern.")]
   public dynamic? Value { get; set; }
 
+  /// <summary>
+  /// The JSON Schema for the <see cref="PatternValue"/> object. This is a generated property and is not meant to be used directly.
+  /// </summary>
   [JsonIgnore]
   [YamlIgnore]
   public JsonSchema JsonSchema => JsonSchema.FromType(GetType());
 
+  /// <summary>
+  /// Converts a <see cref="PatternValue"/> object to a <see cref="string"/>.
+  /// </summary>
+  /// <param name="patternValue"></param>
   public static implicit operator string(PatternValue patternValue)
   {
     if (patternValue.Value != null)
@@ -28,6 +41,10 @@ public class PatternValue : IPatternValue
       return string.Empty;
   }
 
+  /// <summary>
+  /// Converts a <see cref="string"/> to a <see cref="PatternValue"/> object.
+  /// </summary>
+  /// <param name="value"></param>
   public static implicit operator PatternValue(string value)
   {
     return new PatternValue(value);
@@ -63,56 +80,9 @@ public class PatternValue : IPatternValue
     }
   }
 
-
   public PatternValue()
   {
 
-  }
-
-
-  public override string? ToString()
-  {
-    // We want to return the value of the pattern as a string. If the value is another type of pattern, we want to return the value of that pattern as a string.
-    if (Value is PatternValue patternValue)
-    {
-      return patternValue.ToString();
-    }
-    return Value.ToString();
-
-  }
-
-
-  public string SerializeYaml()
-  {
-    var serializer = new SerializerBuilder().Build();
-    var yaml = serializer.Serialize(this);
-    return yaml;
-  }
-
-  public string SerializeJson()
-  {
-    var json = JsonSerializer.Serialize(this);
-    return json;
-  }
-
-  public void DeserializeYaml(string yamlString)
-  {
-    var deserializer = new Deserializer();
-    var pattern = deserializer.Deserialize<PatternValue>(yamlString);
-    if (pattern != null)
-    {
-      Value = pattern.Value ?? string.Empty;
-    }
-  }
-
-  public void DeserializeJson(string jsonString)
-  {
-    var pattern = JsonSerializer.Deserialize<PatternValue>(jsonString) ?? throw new ArgumentException("Invalid JSON");
-
-    if (null != pattern)
-    {
-      Value = pattern.Value ?? string.Empty;
-    }
   }
 
   void IRegexSerializable.DeserializeJson(string jsonString)
@@ -125,6 +95,76 @@ public class PatternValue : IPatternValue
     DeserializeYaml(yamlString);
   }
 
+  /// <summary>
+  /// Deserializes a YAML string into a <see cref="PatternValue"/>
+  /// </summary>
+  /// <returns><see cref="string"/> representing the <see cref="Value"/> of the pattern</returns>
+  public override string? ToString()
+  {
+    // We want to return the value of the pattern as a string. If the value is another type of pattern, we want to return the value of that pattern as a string.
+    if (Value is PatternValue patternValue)
+    {
+      return patternValue.ToString();
+    }
+    return Value.ToString();
+
+  }
+
+  /// <summary>
+  /// Serializes the object to a YAML string
+  /// </summary>
+  /// <returns><see cref="string"/> representing the <see cref="Value"/> of the pattern</returns>
+  public string SerializeYaml()
+  {
+    var serializer = new SerializerBuilder().Build();
+    var yaml = serializer.Serialize(this);
+    return yaml;
+  }
+
+  /// <summary>
+  /// Serializes the object to a JSON string
+  /// </summary>
+  /// <returns><see cref="string"/> representing the <see cref="Value"/> of the pattern</returns>
+  public string SerializeJson()
+  {
+    var json = JsonSerializer.Serialize(this);
+    return json;
+  }
+
+  /// <summary>
+  /// Deserializes a YAML string to this instance of <see cref="PatternValue"/>
+  /// </summary>
+  /// <param name="yamlString"></param>
+  /// <exception cref="ArgumentException"></exception>
+  public void DeserializeYaml(string yamlString)
+  {
+    var deserializer = new Deserializer();
+    var pattern = deserializer.Deserialize<PatternValue>(yamlString);
+    if (pattern != null)
+    {
+      Value = pattern.Value ?? string.Empty;
+    }
+  }
+
+  /// <summary>
+  /// Deserializes a JSON string to this instance of <see cref="PatternValue"/>
+  /// </summary>
+  /// <param name="jsonString"></param>
+  /// <exception cref="ArgumentException"></exception>
+  public void DeserializeJson(string jsonString)
+  {
+    var pattern = JsonSerializer.Deserialize<PatternValue>(jsonString) ?? throw new ArgumentException("Invalid JSON");
+
+    if (null != pattern)
+    {
+      Value = pattern.Value ?? string.Empty;
+    }
+  }
+
+  /// <summary>
+  /// Converts the <see cref="PatternValue"/> to a <see cref="string"/> representation of a Regular Expression.
+  /// </summary>
+  /// <returns></returns>
   public string ToRegex()
   {
     if (Value is PatternValue patternValue)
