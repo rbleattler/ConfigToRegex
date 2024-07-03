e79133ebef74c28644a60824dfb258c3a3566ad0
Rob Bleattler <rbleattler@outlook.com>
2024-07-03 16:06:22 -0400

    refactor: Update Class documentation, and IsJson and IsYaml method documentation in StringUtilities.cs

--- src/ConfigToRegex/Helpers/StringUtilities.cs
+++ src/ConfigToRegex/Helpers/StringUtilities.cs
@@ -3,18 +3,33 @@ using System.Text.RegularExpressions;
 
 namespace ConfigToRegex.Helpers;
 
+/// <summary>
+/// Utilities for basic string manipulation and validation.
+/// </summary>
 public static partial class StringUtilities
 {
+    [GeneratedRegex(Patterns.Yaml)]
+    private static partial Regex MyRegex();
+
+    /// <summary>
+    /// Checks if a string is a JSON object.
+    /// </summary>
+    /// <param name="stringObject"></param>
+    /// <returns> <see cref="bool"/> : True if the string is a JSON object, false otherwise. </returns>
     public static bool IsJson(string stringObject)
     {
         return stringObject.StartsWith('{') && stringObject.EndsWith('}');
     }
 
+    /// <summary>
+    /// Checks if a string is a YAML object.
+    /// </summary>
+    /// <param name="stringObject"></param>
+    /// <returns> <see cref="bool"/> : True if the string is a YAML object, false otherwise. </returns>
     public static bool IsYaml(string stringObject)
     {
         return MyRegex().IsMatch(stringObject);
     }
 
-    [GeneratedRegex(Patterns.Yaml)]
-    private static partial Regex MyRegex();
+
 }
